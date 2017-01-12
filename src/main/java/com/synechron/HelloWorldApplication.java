8cd13ff344d8b276d52dae42e552bcda10c78d5a
Admin <Admin@TRNROOM5-11.synechron.com>
2017-01-12 14:44:27 +0530

    added new request mapping method with REST

--- src/main/java/com/synechron/HelloWorldApplication.java
+++ src/main/java/com/synechron/HelloWorldApplication.java
@@ -2,9 +2,17 @@ package com.synechron;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldApplication {
+	
+	@RequestMapping("/hello")
+	public String sayHello(){
+		return "Hello POD 9...!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
