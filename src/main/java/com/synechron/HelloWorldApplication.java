615135690e8f543a046c2fc1b44e5780ee34b368
Admin <Admin@TRNROOM5-11.synechron.com>
2017-01-12 14:57:20 +0530

    commit from eclipse 2

--- src/main/java/com/synechron/HelloWorldApplication.java
+++ src/main/java/com/synechron/HelloWorldApplication.java
@@ -11,7 +11,7 @@ public class HelloWorldApplication {
 	
 	@RequestMapping("/hello")
 	public String sayHello(){
-		return "Hello POD 9 !";
+		return "Hello Sameer !";
 	}
 
 	public static void main(String[] args) {
